1296670f27e9452c4848a26ff9a8afd162f0e79d
Caroline Martins Alves <carol.petrykowski@hotmail.com>
2022-11-18 09:04:42 -0300

    [ISSUE-2] adiciona servidor socket

--- src/org/replication/ReplicationLibrary.java
+++ src/org/replication/ReplicationLibrary.java
@@ -1,11 +1,20 @@
 package org.replication;
 
+import java.io.IOException;
+
 public class ReplicationLibrary {
-	
+
 	public static void main(String[] args) {
-		ReplicaGroup replicaGroup = new ReplicaGroup();
-		replicaGroup.connectChannel();
-//		ReplicaGroup.getInstance().connectChannel();
+		ReplicaGroup replicaGroup = new ReplicaGroup("Replicator");
+
+		// Servidor Socket
+		int port = Integer.parseInt(args[0]);
+		Server server = new Server(port, replicaGroup);
+		try {
+			server.start();
+		} catch (IOException e) {
+			System.out.println("Erro ao iniciar servidor Socker. Mensagem: " + e.getMessage());
+		}
 	}
 
 }
